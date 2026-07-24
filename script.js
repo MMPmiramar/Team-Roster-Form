ea556156d420fbd19ce76425cf571bde5f9622eb
MMPmiramar <ca303.vendor@minutemanpress.com>
2026-07-24 16:06:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Paste your production Google Web App URL inside these quotes
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw5CJm9AGH9FwQ0lghEdrUxPkU38HeD9xeKS_EIef5KihyZuFdvZ7ZLcgfsrtMJWTx7TA/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzC6vaFudQOJL9yPLgJ0XQ6LsJUX8Mn6JDm7PXMLmnMQeON-4OagLJPhL7mhbo9I97M-A/exec";
 const playerContainer = document.getElementById('playerContainer');
 const addPlayerBtn = document.getElementById('addPlayerBtn');
 const previewModal = document.getElementById('previewModal');
